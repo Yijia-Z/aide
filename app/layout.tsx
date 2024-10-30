3de4bcd7ba367c7d3d54e6fd4577f8f8382215c5
赵驿嘉 <zhaoyijia@idea.edu.cn>
2024-10-30 11:30:33 +0800

    hotfix: add back mobile scale restriction

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
